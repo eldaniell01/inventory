f8ea2484b29beaaacf302365f1b373339b4f1ed8
eldaniell01 <amontepequem@miumg.edu.gt>
2025-01-16 23:19:11 -0600

    inventario: registro de marcas

--- db/querys.py
+++ db/querys.py
@@ -12,3 +12,19 @@ class Query:
         values = (nit, nombre)
         self.db.execute_query(query, values)
         self.db.close_connection()
+        
+    def insertMarca(self, marca, proveedor):
+        query = """
+                    INSERT INTO marca(nombre, idproveedores) VALUES(%s, %s)
+                """
+        values = (marca, proveedor)
+        self.db.execute_query(query, values)
+        self.db.close_connection()
+        
+    def selectProveedor(self):
+        query = """
+                    SELECT idProveedores, nombre FROM proveedores ORDER BY idProveedores
+                """
+        result = self.db.execute_query(query)
+        self.db.close_connection()
+        return result
--- views/home.py
+++ views/home.py
@@ -10,8 +10,13 @@ class Home(QMainWindow):
         self.main = uic.loadUi('views/home.ui')
         self.main.show()
         self.main.btnProveedor.clicked.connect(self.registerProveedor)
+        self.main.btnMarca.clicked.connect(self.registerMarca)        
         self.main.btnClean.clicked.connect(self.cleanTables)
+        self.main.btnClean2.clicked.connect(self.cleanTables)
         self.showTProveedor()
+        self.showTMarca()
+        self.proveedor = []
+        self.showProveedor()
         
     def showTProveedor(self):
         columns = ['NIT', 'PROVEEDOR']
@@ -32,6 +37,33 @@ class Home(QMainWindow):
         }
         """
         self.main.tProveedor.setStyleSheet(header_style)
+    
+    def showTMarca(self):
+        columns = ['MARCA', 'PROVEEDOR']
+        self.main.tMarca.setFont(QFont("FiraCode Nerd Font", 12))
+        self.main.tMarca.setColumnCount(len(columns))
+        for column, name in enumerate(columns):
+            self.main.tMarca.setHorizontalHeaderItem(column, QTableWidgetItem(name))
+        self.main.tMarca.horizontalHeader().setSectionResizeMode(QHeaderView.ResizeMode.Stretch)
+        header_style = """
+        QHeaderView::section {
+            font-family: "FiraCode Nerd Font";
+            font-size: 12pt;
+            font-weight: bold;
+            background-color: rgb(255, 255, 255);
+        }
+        QTableWidget{
+            background-color: rgb(255, 255, 255);
+        }
+        """
+        self.main.tMarca.setStyleSheet(header_style)
+        
+    def showProveedor(self):
+        query = Query()
+        options = set(self.proveedor)
+        result = query.selectProveedor()
+        for id, data in result:
+            self.main.cbProveedor.addItem(str(data))
         
     def registerProveedor(self):
         query = Query()
@@ -52,6 +84,26 @@ class Home(QMainWindow):
                 self.main.tProveedor.setItem(row, 1, QTableWidgetItem(self.nombre))
                 self.main.txtNIT.setText("")
                 self.main.txtProveedor.setText("")
+    
+    def registerMarca(self):
+        query = Query()
+        idProveedor = self.main.cbProveedor.currentIndex()+1
+        self.marca = self.main.txtMarca.text()
+        if idProveedor:
+            message_box = QMessageBox(self)
+            message_box.setWindowTitle("Confirmación")
+            message_box.setText("¿Estás seguro de que deseas continuar?")
+            message_box.setStandardButtons(QMessageBox.StandardButton.Yes | QMessageBox.StandardButton.No)
+            message_box.setIcon(QMessageBox.Icon.Question)
+            response = message_box.exec()
+            if response == QMessageBox.StandardButton.Yes:
+                query.insertMarca(self.marca, idProveedor)
+                row = self.main.tMarca.rowCount()
+                self.main.tMarca.insertRow(row)
+                self.main.tMarca.setItem(row, 0, QTableWidgetItem(self.marca))
+                self.main.tMarca.setItem(row, 1, QTableWidgetItem(self.main.cbProveedor.currentText()))
+                self.main.txtMarca.setText("")
+                self.main.cbProveedor.setCurrentIndex(-1)
             
         
     def cleanTables(self):
