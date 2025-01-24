9f44ec8872327b00f25713523744b09c9af82b13
eldaniell01 <amontepequem@miumg.edu.gt>
2025-01-23 23:55:44 -0600

    modificacion de la base de datos y metodo de carga de datos de excel

--- views/home.py
+++ views/home.py
@@ -1,4 +1,6 @@
 from PyQt6 import uic
+import os
+from openpyxl import load_workbook
 from PyQt6.QtWidgets import QApplication, QMainWindow, QWidget, QMessageBox, QFileDialog, QTableWidget, QHeaderView, QTableWidgetItem
 from PyQt6.QtGui import QFont
 from db.conexion import ConexionMysql
@@ -13,8 +15,10 @@ class Home(QMainWindow):
         self.main.btnMarca.clicked.connect(self.registerMarca)        
         self.main.btnClean.clicked.connect(self.cleanTables)
         self.main.btnClean2.clicked.connect(self.cleanTables)
+        self.main.btnList.clicked.connect(self.openExcel)
         self.showTProveedor()
         self.showTMarca()
+        self.showTcompra()
         self.proveedor = []
         self.showProveedor()
         
@@ -58,13 +62,59 @@ class Home(QMainWindow):
         """
         self.main.tMarca.setStyleSheet(header_style)
         
+    def showTcompra(self):
+        columns = ['CANTIDAD', 'CODIGO', 'DESCRIPCION', 'MEDIDA', 'PRECIO C.', 'PRECIO V.', 'DESCUENTO', 'SUBTOTAL' ]
+        self.main.tShop.setFont(QFont("FiraCode Nerd Font", 12))
+        self.main.tShop.setColumnCount(len(columns))
+        for column, name in enumerate(columns):
+            self.main.tShop.setHorizontalHeaderItem(column, QTableWidgetItem(name))
+        self.main.tShop.horizontalHeader().setSectionResizeMode(QHeaderView.ResizeMode.Stretch)
+        header_style = """
+        QHeaderView::section {
+            font-family: "FiraCode Nerd Font";
+            font-size: 12pt;
+            font-weight: bold;
+            background-color: rgb(255, 255, 255);
+        }
+        QTableWidget{
+            background-color: rgb(255, 255, 255);
+        }
+        """
+        self.main.tShop.setStyleSheet(header_style)    
+    
     def showProveedor(self):
         query = Query()
         options = set(self.proveedor)
         result = query.selectProveedor()
         for id, data in result:
             self.main.cbProveedor.addItem(str(data))
+            self.main.cbProveedor_2.addItem(str(data))
         
+    def loadExcel(self,path):
+        workbook = load_workbook(filename=path)
+        sheet = workbook.active
+        for row in sheet.iter_rows(values_only= True):
+            row_index = self.main.tShop.rowCount()
+            self.main.tShop.insertRow(row_index)
+            print(row[1])
+            self.main.tShop.setItem(row_index, 0, QTableWidgetItem(str(row[0])))#cantidad
+            self.main.tShop.setItem(row_index, 1, QTableWidgetItem(str(row[1]).upper()))#codigo
+            self.main.tShop.setItem(row_index, 2, QTableWidgetItem(str(row[2]).upper()))#nombre o descripcion
+            self.main.tShop.setItem(row_index, 3, QTableWidgetItem(str(row[3]).upper()))#unidad de medida
+            self.main.tShop.setItem(row_index, 4, QTableWidgetItem(str(row[4])))#precio de compra
+            self.main.tShop.setItem(row_index, 5, QTableWidgetItem(str(row[5])))#precio de venta          
+            self.main.tShop.setItem(row_index, 6, QTableWidgetItem(str(row[6])))#precio de descuento
+            self.main.tShop.setItem(row_index, 7, QTableWidgetItem(str(row[7])))#subtotal
+            
+    def openExcel(self):
+        folder = QFileDialog()
+        try: 
+            folder_path, __= folder.getOpenFileName(None, 'ABRIR ARCHIVO', '', 'xlsx (*.xlsx)')
+            print(folder_path)
+            self.loadExcel(folder_path)
+        except Exception as e:
+            print(e)
+          
     def registerProveedor(self):
         query = Query()
         self.nit = self.main.txtNIT.text()
