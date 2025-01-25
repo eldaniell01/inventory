45f29a2f1b474f8d2801b68c622aa67dcefe4620
eldaniell01 <amontepequem@miumg.edu.gt>
2025-01-25 00:36:44 -0600

    querys para facturas las compras y actualizar el inventario

--- db/querys.py
+++ db/querys.py
@@ -21,6 +21,30 @@ class Query:
         self.db.execute_query(query, values)
         self.db.close_connection()
         
+    def insertProducto(self, cod, nombre, stock, preciov, medida, marca):
+        query = """
+                    INSER INTO productos(codigo, nombre, stock, precio_v, unidad_m, marca) VALUES(%s, %s, %s, %s, %s, %s)
+                """
+        values = (cod, nombre, stock, preciov, medida, marca)
+        self.db.execute_query(query, values)
+        self.db.close_connection()
+        
+    def insertDetailCompra(self, cantidad, idP, precioc, desc, subtotal, idEncabezado):
+        query = """
+                    INSERT INTO detalle_compra(cantidad, idproducto, precio_c, descuento, sub_total, idencabezado_factura) VALUES(%s, (SELECT idproducto FROM productos WHERE codigo=%s LIMIT 1), %s, %s, %s, %s)
+                """
+        values = (cantidad, idP, precioc, desc, subtotal, idEncabezado)
+        self.db.execute_query(query, values)
+        self.db.close_connection()
+        
+    def insertEncabezadoCompra(self, date, state, total, idT, idProveedor):
+        query = """
+                    INSERT INTO encabezado_factura(fecha, estado, total, idtienda, idproveedores) VALUES(%s, %s, %s, %s, %s)
+                """
+        values = (date, state, total, idT, idProveedor)
+        self.db.execute_query(query, values)
+        self.db.close_connection()
+        
     def selectProveedor(self):
         query = """
                     SELECT idProveedores, nombre FROM proveedores ORDER BY idProveedores
